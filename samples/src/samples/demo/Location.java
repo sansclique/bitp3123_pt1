f9a82b8e9a53b26a8983eff5015265ca58136f34
najwa <najwashaharudinoj@gmail.com>
2023-04-10 12:27:34 +0800

    Adding one private member, getter and setter

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,17 @@
+package samples.demo;
+
+
+
+public class Location {
+	
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
